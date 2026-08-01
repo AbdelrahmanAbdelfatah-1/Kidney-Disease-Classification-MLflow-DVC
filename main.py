4e72f1c3f30eeb6ef298b8adb6bed26cd3939b94
AbdelrahmanAbdelfatah-1 <abedomahmed@gmail.com>
2026-08-01 21:08:30 +0300

    utils added

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.cnnClassifier import logger
+
+logger.info("Starting the CNN Classifier application...")
+logger.warning("This is a warning message.")
+logger.error("This is an error message.")
